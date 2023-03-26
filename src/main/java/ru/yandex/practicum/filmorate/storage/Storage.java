9296f01d20e3c6a904f94071c561126727b52c80
Kaminari <Opk26rus@yandex.ru>
2023-03-26 18:09:58 +0300

    update 2.8

--- src/main/java/ru/yandex/practicum/filmorate/storage/Storage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/Storage.java
@@ -4,8 +4,12 @@ import java.util.Collection;
 
 public interface Storage<T> {
     T add(T obj);
+
     T update(T obj);
+
     T getById(Long id);
+
     void delete(Long id);
+
     Collection<T> getAll();
 }
