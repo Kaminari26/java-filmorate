c7171c16e56dd05f8ef1cb113dcc630b8fe6334b
Kaminari <Opk26rus@yandex.ru>
2023-04-23 04:02:16 +0300

    SAVE ME PLEASE 3

--- src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
+++ src/test/java/ru/yandex/practicum/filmorate/FilmorateApplicationTests.java
@@ -1,34 +1,34 @@
-package ru.yandex.practicum.filmorate;
-
-import lombok.RequiredArgsConstructor;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.context.SpringBootTest;
-import ru.yandex.practicum.filmorate.model.User;
-import ru.yandex.practicum.filmorate.storage.user.UserStorage;
-
-import java.util.Optional;
-
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-
-@SpringBootTest
-@AutoConfigureTestDatabase
-@RequiredArgsConstructor(onConstructor = @__(@Autowired))
-class FilmorateApplicationTests {
-
-    private final UserStorage userStorage;
-
-    @Test
-    public void testFindUserById() {
-
-        Optional<User> userOptional = Optional.of(userStorage.getById(1L));
-
-        assertThat(userOptional)
-                .isPresent()
-                .hasValueSatisfying(user ->
-                        assertThat(user).hasFieldOrPropertyWithValue("id", 1)
-                );
-    }
-
-}
+//package ru.yandex.practicum.filmorate;
+//
+//import lombok.RequiredArgsConstructor;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import ru.yandex.practicum.filmorate.model.User;
+//import ru.yandex.practicum.filmorate.storage.user.UserStorage;
+//
+//import java.util.Optional;
+//
+//import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+//
+//@SpringBootTest
+//@AutoConfigureTestDatabase
+//@RequiredArgsConstructor(onConstructor = @__(@Autowired))
+//class FilmorateApplicationTests {
+//
+//    private final UserStorage userStorage;
+//
+//    @Test
+//    public void testFindUserById() {
+//
+//        Optional<User> userOptional = Optional.of(userStorage.getById(1L));
+//
+//        assertThat(userOptional)
+//                .isPresent()
+//                .hasValueSatisfying(user ->
+//                        assertThat(user).hasFieldOrPropertyWithValue("id", 1)
+//                );
+//    }
+//
+//}
